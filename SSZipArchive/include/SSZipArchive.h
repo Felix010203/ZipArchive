690d55f4082ca97045bfe95e31f85eeb353abd25
Matthias Felix <felix@ubique.ch>
2021-03-16 09:06:22 +0100

    Fix SPM build

--- SSZipArchive/include/SSZipArchive.h
+++ SSZipArchive/include/SSZipArchive.h
@@ -10,12 +10,7 @@
 #define _SSZIPARCHIVE_H
 
 #import <Foundation/Foundation.h>
-
-#if COCOAPODS
-#import <SSZipArchive/SSZipCommon.h>
-#else
-#import <ZipArchive/SSZipCommon.h>
-#endif
+#import "SSZipCommon.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
